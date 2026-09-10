395d115877e1386630f54696eb782180a3134548
kdbroskii <kdbroskii@gmail.com>
2026-09-10 22:46:53 +0100

    Initialize Form1 with basic properties

--- app/Form1.Designer.cs
+++ app/Form1.Designer.cs
@@ -1,1 +1,22 @@
+namespace MyGameBot;
 
+partial class Form1
+{
+    private System.ComponentModel.IContainer components = null;
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+            components.Dispose();
+        base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+        this.components = new System.ComponentModel.Container();
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(1280, 720);
+        this.Text = "MyGameBot";
+        this.Name = "Form1";
+    }
+}

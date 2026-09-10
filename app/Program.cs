10cb042fa271723ff345023503e4f112bc9192ff
kdbroskii <kdbroskii@gmail.com>
2026-09-10 22:46:27 +0100

    Initialize application and run main form

--- app/Program.cs
+++ app/Program.cs
@@ -1,1 +1,11 @@
+namespace MyGameBot;
 
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}

3468e06057d188ebe1733f864e8f9bcd187bc85f
kdbroskii <kdbroskii@gmail.com>
2026-09-11 06:28:00 +0100

    Add ResourceProbe class with farming method

--- play/ResourceProbe.cs
+++ play/ResourceProbe.cs
@@ -1,1 +1,13 @@
+using System.Numerics;
 
+namespace MyGameBot;
+
+public class ResourceProbe
+{
+    public void Farm(IGameState s, IActionAPI a, Context ctx)
+    {
+        a.MoveForward();
+        a.EquipPickaxe();
+        _ = ctx;
+    }
+}

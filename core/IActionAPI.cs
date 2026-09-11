4f72fce6d5f3be86e655c4c2cc5062bec5ae228f
kdbroskii <kdbroskii@gmail.com>
2026-09-11 06:25:21 +0100

    Add IActionAPI interface for game actions

--- core/IActionAPI.cs
+++ core/IActionAPI.cs
@@ -1,1 +1,42 @@
+namespace MyGameBot;
 
+public interface IActionAPI
+{
+    // Movement
+    void MoveForward();
+    void MoveBack();
+    void MoveLeft();
+    void MoveRight();
+    void Jump();
+    void Sprint(bool on);
+    void CrouchSlide(bool hold);
+    void AutoRun(bool on);
+    void Stop();
+
+    // Combat
+    void Fire();
+    void ADS(bool on);
+    void Reload();
+    void Interact();
+    void EquipPickaxe();
+    void SelectSlot(int slot);
+
+    // Building
+    void SelectWall();
+    void SelectFloor();
+    void SelectStairs();
+    void SelectCone();
+    void SelectTrap();
+    void RotatePiece();
+    void ChangeMaterial();
+    void PlaceBuild();
+    void ConfirmBuild();
+    void ResetBuild();
+
+    // Editing
+    void EnterEdit();
+    void SelectEditTile(Vector2Int cell);
+    void ConfirmEdit();
+    void ResetEdit();
+    void ExitEdit();
+}

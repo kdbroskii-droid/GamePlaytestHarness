32231554e54a4a8391d6c22b2022038361debc59
kdbroskii <kdbroskii@gmail.com>
2026-09-11 06:24:58 +0100

    Define IGameState interface with game state properties

--- core/IGameState.cs
+++ core/IGameState.cs
@@ -1,1 +1,30 @@
+using System.Collections.Generic;
+using System.Numerics;
 
+namespace MyGameBot;
+
+public interface IGameState
+{
+    Vector3 PlayerPosition { get; }
+    Vector3 PlayerVelocity { get; }
+    float   PlayerHealth   { get; }
+    float   PlayerShield   { get; }
+    int     Wood           { get; }
+    int     Brick          { get; }
+    int     Metal          { get; }
+
+    IReadOnlyList<ITargetable> Enemies   { get; }
+    IReadOnlyList<Vector3>     StormPath { get; }
+
+    bool  IsReloading { get; }
+    bool  IsEditing   { get; }
+    bool  IsBuilding  { get; }
+    int   AmmoInMag   { get; }
+    float WeaponRange { get; }
+
+    Vector3 CameraForward { get; }
+
+    IBuildGrid BuildGrid { get; }
+    bool HasBuildPieceAt(Vector3 pos);
+    PieceType? GetBuildPieceAt(Vector3 pos);
+}
